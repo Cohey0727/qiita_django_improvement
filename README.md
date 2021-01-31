b5e3fef49876b3a814ee9004c309436a851988e5
Kohei <ohayousagi.ac.kook0727@gmail.com>
2021-01-31 16:37:51 +0900

    master

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
-python manage.py loaddata dump.json
+python manage.py runscripts query.fixture
 touch logs/query.log
 ```
 
---
+++ query/fixture.py
@@ -0,0 +1,45 @@
+import random
+from tqdm import tqdm
+
+from .models import *
+
+GROUP_COUNT = 10000
+USER_COUNT_BY_GROUP = 100
+INVOICE_COUNT_BY_USER = 10
+BATCH_SIZE = 500
+
+ALL_RECORD = GROUP_COUNT + GROUP_COUNT * USER_COUNT_BY_GROUP + GROUP_COUNT * USER_COUNT_BY_GROUP * INVOICE_COUNT_BY_USER
+
+
+def run():
+    done_records = 0
+
+    # create groups
+    groups = [Group(name=f'group-{index}') for index in range(GROUP_COUNT)]
+    Group.objects.bulk_create(groups, batch_size=BATCH_SIZE)
+    done_records = GROUP_COUNT
+
+    # create users
+    all_groups = Group.objects.all()
+    users = [
+        User(name=f'group-{group.id}_user-{index}', group=group)
+        for index in range(USER_COUNT_BY_GROUP) for group in all_groups
+    ]
+    User.objects.bulk_create(users, batch_size=BATCH_SIZE)
+    done_records += USER_COUNT_BY_GROUP * GROUP_COUNT
+
+    # create invoices
+    all_users = User.objects.all()
+    invoices = []
+    for user in tqdm(all_users):
+        invoices.extend([
+            Invoice(price=random.randint(1000, 100000), user=user)
+            for index in range(INVOICE_COUNT_BY_USER)
+        ])
+        if len(invoices) >= BATCH_SIZE:
+            Invoice.objects.bulk_create(invoices, batch_size=BATCH_SIZE)
+            done_records += len(invoices)
+            invoices = []
+
+    Invoice.objects.bulk_create(invoices, batch_size=BATCH_SIZE)
+    done_records += len(invoices)
